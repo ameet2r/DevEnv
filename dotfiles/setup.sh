c87e4d1dc60862d124006497095a3aa8343b379e
Ameet Toor <Ameet_Toor@Hotmail.com>
2025-03-04 23:25:32 -0800

    The previous test of not using symlink worked, removing other symlinks

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -6,8 +6,8 @@ export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
 # Create symlinks for configurations
-ln -sF "opt/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sF "$PWD/tmux.conf" "$HOME"/tmux.conf
+# ln -sF "opt/nvim" "$XDG_CONFIG_HOME"/nvim
+# ln -sF "$PWD/tmux.conf" "$HOME"/tmux.conf
 # ln -sF "$PWD/.vimrc" "$HOME"/.vimrc
 # ln -sF "$PWD/.zshrc" "$HOME"/.zshrc
 
