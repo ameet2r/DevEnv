7a44b838eac57daf0e1d490f9ff2db8653f8cbe1
Ameet Toor <Ameet_Toor@Hotmail.com>
2025-03-05 07:16:31 -0800

    Update comments

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -5,12 +5,6 @@
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-# Create symlinks for configurations
-# ln -sF "opt/nvim" "$XDG_CONFIG_HOME"/nvim
-# ln -sF "$PWD/tmux.conf" "$HOME"/tmux.conf
-# ln -sF "$PWD/.vimrc" "$HOME"/.vimrc
-# ln -sF "$PWD/.zshrc" "$HOME"/.zshrc
-
 # TODO maybe do something with cht.sh and tmux-sessionizer if needed
 
 packages=(
@@ -28,8 +22,5 @@ done
 echo "All pacakges have been installed."
 
 source "$HOME"/.zshrc
-
-# TODO Install oh-my-zsh
-# TODO setup completions, if not already done by by .zshrc file
 
 echo "Setup complete."
